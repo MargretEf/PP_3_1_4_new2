e48d404e6c286d88f5dc4f791dbe49c60266f3a0
Your Name <jgjerogk@mail.ru>
2024-08-06 08:57:51 +0300

    first commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,76 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.models.Role;
+import ru.kata.spring.boot_security.demo.models.User;
+import ru.kata.spring.boot_security.demo.repositories.UserRepository;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImp implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
+
+    @Autowired
+    public UserServiceImp(UserRepository peopleService, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+        this.passwordEncoder = passwordEncoder;
+        this.userRepository = peopleService;
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User getUser(long id) {
+        return userRepository.getById(id);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Transactional
+    @Override
+    public void register(User user) {
+        Set<Role> roles = new HashSet<>();
+        for (Role role : user.getRole()) {
+            roles.add(roleRepository.findById(role.getId()).get());
+        }
+        user.setRole(roles);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @Override
+    public void edit(User user) {
+        if (user == null) {
+            throw new EntityNotFoundException("User with ID " + user.getId() + " not found");
+        }
+        //Это нужно, чтоб пароль не хэшировался каждый раз, когда редактируем юзера
+        //Проверяет, если пароль совпадает с текущим - сохраняет, если нет - шифрует
+        var currentPassword = userRepository.findById(user.getId()).get().getPassword();
+        if (user.getPassword().equals(currentPassword)) {
+            userRepository.save(user);
+        } else {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            userRepository.save(user);
+        }
+    }
+}
